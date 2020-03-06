c4771f9c7934de56808084d74b4f6c2dea7cbce2
Raj Patel <rpatelpj@gmail.com>
2020-03-06 17:09:09 -0500

    Create wrapper class, example use, import necessary submodules

---
+++ Arduino-ESP8266-Firebase-wrapper.ino
@@ -0,0 +1,40 @@
+#include "AEF.h"
+
+AEF* aef;
+
+void setup() {
+  // Example class initialization
+  // char* FirebaseHost = "example.firebaseio.com";
+  // char* FirebaseAuth = "secret-key";
+  // char* WiFiSSID = "redacted";
+  // char* WiFiPassword = "redacted";
+  // AEF* aef = new AEF(FirebaseHost, FirebaseAuth, WiFiSSID, WiFiPassword);
+}
+
+void loop() {
+  // Example Firebase data
+  // char* transmitID = "folder/tVar";
+  // char* receiveID = "rVar";
+  
+  // Example class use to update Firebase
+  // bool success = false;
+  // pinMode(2, OUTPUT);
+  // digitalWrite(2, HIGH);
+  // while (!success) {
+  //   success = aef->transmitData(transmitID);
+  //   delay(500);
+  // }
+
+  // Example class use to pull from Firebase
+  // int data = -1;
+  // while (data == -1) {
+  //   data = aef->receiveData(receiveID);
+  // }
+  // if (data > 0) {
+  //   pinMode(2, OUTPUT);
+  //   digitalWrite(2, HIGH);
+  // } else if (data == 0) {
+  //   pinMode(2, OUTPUT);
+  //   digitalWrite(2, LOW);
+  // }
+}
